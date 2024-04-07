80fd1d82fab5cb3fc70e0095df21705bb8fc9285
Akash Thakur <117273921+akashthakur4553@users.noreply.github.com>
2024-04-07 10:55:18 +0530

    added files

---
+++ app.py
@@ -0,0 +1,35 @@
+from flask import *
+from pyresparser import ResumeParser
+import os
+
+# from docx import Document
+import nltk
+import spacy
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def main():
+    return render_template("index.html")
+
+
+@app.route("/upload", methods=["POST"])
+def upload():
+    if request.method == "POST":
+        data_l = []
+        # Get the list of files from webpage
+        files = request.files.getlist("file")
+
+        # Iterate for each file in the files List, and Save them
+        for file in files:
+            file.save(file.filename)
+            filed = file.filename
+            data = ResumeParser(filed).get_extracted_data()
+            data_l.append(data["name"])
+
+        return f"<h1>Files Uploaded Successfully.!</h1> {data_l}"
+
+
+# if __name__ == "__main__":
+#     app.run(debug=True)
